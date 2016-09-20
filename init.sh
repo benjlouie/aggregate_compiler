b88c41853a5c966afe7dc3eee944a2154352e202
Matthew Karasz <mattchris9@hotmail.com>
2016-09-20 16:32:15 -0600

    Init works now :D

--- init.sh
+++ init.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 git submodule init
+git submodule update
+
 MYDIR="../aggregate_compiler"
 DIRS=`ls --time-style="long-iso"  -l ../aggregate_compiler | egrep '^d' | awk '{print $8}'`
 
-for DIR in $DIRS; do
-	
+for DIR in $DIRS; do	
 	cd $DIR
 	git checkout master && git pull
 	cd ..
