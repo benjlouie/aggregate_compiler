04d52073dcda51ea34157a9a3f86c31f9331116f
benjlouie <benjlouie@gmail.com>
2016-12-05 14:20:00 -0700

    created the needed methods to unswitch a loop in the AST

--- src/tree.cpp
+++ src/tree.cpp
@@ -85,10 +85,27 @@ bool Tree::replaceChild(Tree *oldChild, Tree *newChild)
 	for (size_t i = 0; i < children.size(); i++) {
 		if (children[i] == oldChild) {
 			children[i] = newChild;
+			newChild->parent = this;
 			return true;
 		}
 	}
 	return false;
+}
+
+bool Tree::replaceDescendant(vector<size_t>& path, Tree * newChild)
+{
+	//follow path down
+	Tree *cur = this;
+	for (size_t childInd : path) {
+		if (childInd >= cur->children.size()) {
+			//error, ind is bad
+			//throw "Tree::replaceDescendant(): child index too high, bad path";
+			return false;
+		}
+		cur = cur->children[childInd];
+	}
+
+	return cur->replaceSelf(newChild);
 }
 
 bool Tree::deleteChild(Tree *child)
@@ -110,6 +127,60 @@ bool Tree::replaceSelf(Tree *newNode)
 bool Tree::deleteSelf(void)
 {
 	return this->parent->deleteChild(this);
+}
+
+vector<size_t> Tree::getPath(Tree *descendant)
+{
+	vector<size_t> pathReversed;
+	//follow descendant up to ancestor
+	while (descendant && descendant != this) {
+		Tree *parent = descendant->parent;
+		//get child index
+		auto &kids = parent->children;
+		size_t i = 0;
+		for (; i < kids.size(); i++) {
+			if (kids[i] == descendant) {
+				pathReversed.push_back(i);
+				descendant = parent;
+				break;
+			}
+		}
+		if (i == kids.size()) {
+			//wasn't in parent children..ERROR
+			throw "Tree::getPath(): child not in parent childList";
+		}
+	}
+	if (descendant != this) {
+		//did not get to ancestor
+		throw "Tree::getPath(): descendant not beneath ancestor";
+	}
+
+	//reverse the path
+	std::reverse(pathReversed.begin(), pathReversed.end());
+
+	return pathReversed;
+}
+
+Tree * Tree::deepCopy(void)
+{
+	throw "Tree::deepCopy(): not implemented lol";
+	return nullptr;
+}
+
+bool Tree::setChild(size_t childInd, Tree *newChild)
+{
+	if (childInd > this->children.size()) {
+		//2+ over can't do it
+		return false;
+	}
+	if (childInd == this->children.size()) {
+		//at the end, just append
+		this->addChild(newChild);
+		return true;
+	}
+	//in the middle, replace
+	Tree *oldChild = this->children[childInd];
+	return this->replaceChild(oldChild, newChild);
 }
 
 vector<Tree *> Tree::getChildren()
